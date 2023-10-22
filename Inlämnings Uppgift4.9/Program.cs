1e16d85e3073b8cb108a4a6a6f90e5320f1a2d1c
baronvonpurr <robinhood.svensson@hotmail.com>
2023-10-22 17:57:59 +0200

    Add project files.

---
+++ Inlämnings Uppgift4.9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uppgift4_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tecken = "";
+            Console.WriteLine("hur stor ska sidlängden på triangeln vara?");
+            int bredd = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < bredd; i++)
+            {
+                tecken = tecken + "*";
+                Console.WriteLine(tecken);
+            }
+            Console.ReadKey();
+        }
+    }
+}
